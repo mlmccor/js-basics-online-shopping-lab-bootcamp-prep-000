9647217c99563a27a62fe6820320c0bea553fd17
Matthew McCormack <mmccormack1182@gmail.com>
2018-04-29 20:13:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ function removeFromCart(item) {
 }
 function placeOrder(cardNumber) {
   var sum= total()
- if(cardNumber===0) {
+ if(cardNumber===undefined) {
    return `Sorry, we don't have a credit card on file for you.`
  } else {
    return `Your total cost is ${total}, which will be charged to the card ${cardNumber}.`
