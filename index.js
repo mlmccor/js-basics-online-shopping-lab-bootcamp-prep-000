b2234b7b505b08b02dec197af2febae868ed9ea3
Matthew McCormack <mmccormack1182@gmail.com>
2018-04-29 20:17:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -65,6 +65,7 @@ function placeOrder(cardNumber) {
  if(cardNumber===undefined) {
    return `Sorry, we don't have a credit card on file for you.`
  } else {
+   cart.length=0
    return `Your total cost is $${sum}, which will be charged to the card ${cardNumber}.`
- } cart.length=0
+ } 
 }
